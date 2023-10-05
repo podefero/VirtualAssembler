45d981480c5513375bc205bfa26ceadc0d51a757
podefero <naynayrjones@gmail.com>
2023-10-04 19:34:54 -0600

    removed debugging output

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,15 @@
 int main(int argc, char *argv[]) {
   std::string filePath = "";
 
-  if(argc == 1) {
-    //fixed path
+  if (argc == 1) {
+    // fixed path
     filePath = "../tests/HelloWorld.bin";
-  }
-  else if (argc == 2) {
+  } else if (argc == 2) {
     filePath = argv[1];
   } else {
     std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
     return 1;
   }
-
 
   try {
     VirtualMachine vm;
@@ -25,13 +23,13 @@ int main(int argc, char *argv[]) {
       vm.initPc();
       int memory_size = vm.findTrap0();
       // if we can't validate code segment then exit;
-      if (memory_size == 1) 
+      if (memory_size == 1)
         return 1;
       // std::cout << memory_size << std::endl;
 
-      //validate opcodes by fetch and decode, the second pass we will execute.
+      // validate opcodes by fetch and decode, the second pass we will execute.
       while (memory_size > vm.memory.pc) {
-        if (vm.fetch() != 1){
+        if (vm.fetch() != 1) {
           std::cerr << "failed to fetch";
           return 1;
         }
@@ -41,17 +39,17 @@ int main(int argc, char *argv[]) {
         }
       }
 
-      vm.initPc(); //reset pc
-      //execution process. second pass.
+      vm.initPc(); // reset pc
+      // execution process. second pass.
       while (memory_size > vm.memory.pc) {
         if (vm.fetch() != 1)
           std::cerr << "failed to fetch";
         if (vm.decode() != 1)
           std::cerr << "failed to decode";
         int result = vm.execute();
-        if (result == 2)
-          std::cout << std::endl << "Successful exit" << std::endl;
-        else if (result != 1) {
+        // if (result == 2)
+        //  std::cout << std::endl << "Successful exit" << std::endl;
+        if (result == -1) {
           std::cout << "Execute failed " << std::endl;
         }
       }
