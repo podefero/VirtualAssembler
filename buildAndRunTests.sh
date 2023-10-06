e2e0e4c394844694886373a49c944dba6fcdf722
podefero <naynayrjones@gmail.com>
2023-10-05 18:59:35 -0600

    removed unsued files

--- buildAndRunTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Nathaniel Jones
-# Automate building cmake stuff
-cmake -S . -B build
-read -p "Press Enter to continue.."
-cmake --build build
-read -p "Press Enter to test.."
-cd build && ctest
