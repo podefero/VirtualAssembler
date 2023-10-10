daab9a7fd159a4dd170ca524b47f2074131bf6b7
podefero <naynayrjones@gmail.com>
2023-10-10 16:29:01 -0600

    creating design. updated trap3 to read byte

--- src/OperationTrap3.cpp
+++ src/OperationTrap3.cpp
@@ -9,8 +9,8 @@ OperationTrap3::OperationTrap3(int op1, int op2) {
 int OperationTrap3::validate(Memory &memory) { return 1; }
 
 int OperationTrap3::execute(Memory &memory) {
-  // throw(TrapException("TRAP INTERRUPT"));
   int r3 = memory.registers.getRegister(3);
-  std::cout << char(r3);
+  char byte = static_cast<char>(r3 & 0xFF);
+  std::cout << byte;
   return 3; // return 3 for trap 3
 }
